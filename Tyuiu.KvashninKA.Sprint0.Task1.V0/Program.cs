e0365532ff19886553b435dfcd190e19707b0d50
KvashninKA <kirillkvasnin55@gmail.com>
2026-09-13 18:13:39 +0500

    Task1

---
+++ Tyuiu.KvashninKA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KvashninKA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Кирилл";
+            int age = 19;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+        }
+    }
+}

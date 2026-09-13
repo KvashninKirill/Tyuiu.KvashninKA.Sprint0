d3087ebab78398e832ec6bb34bdd5cd2eedfe981
KvashninKA <kirillkvasnin55@gmail.com>
2026-09-13 18:58:52 +0500

    Task2

---
+++ Tyuiu.KvashninKA.Sprint0.Task2.V0.Test/DataServiseTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KvashninKA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.KvashninKA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiseTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Кирилл";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Кирилл", res);
+        }
+    }
+}

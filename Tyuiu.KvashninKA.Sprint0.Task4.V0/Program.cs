174292157c15613733026cb7cafd1c1566acccce
KvashninKA <kirillkvasnin55@gmail.com>
2026-09-13 20:16:13 +0500

    Task4

--- Tyuiu.KvashninKA.Sprint0.Task4.V0/Program.cs
+++ Tyuiu.KvashninKA.Sprint0.Task4.V0/Program.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.KvashninKA.Sprint0.Task3.V0
             Console.WriteLine(DataService.Addition(1, 5));
             Console.WriteLine(DataService.Subtraction(15, 5));
             Console.WriteLine(DataService.Multiplication(10, 10));
-            Console.WriteLine(DataService.Division(5, 5));
+            Console.WriteLine(DataService.Division(10, 10));
             Console.ReadKey();
         }
     }

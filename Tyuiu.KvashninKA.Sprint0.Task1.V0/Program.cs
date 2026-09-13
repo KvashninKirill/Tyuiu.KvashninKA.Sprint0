6936578f3cd335a5c56fc8e61fb6496d951c9180
KvashninKA <kirillkvasnin55@gmail.com>
2026-09-13 18:21:15 +0500

    Task1

--- Tyuiu.KvashninKA.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.KvashninKA.Sprint0.Task1.V0/Program.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.KvashninKA.Sprint0.Task1.V0
         {
             Console.WriteLine("Hello, World");
             string name = "Кирилл";
-            int age = 19;
+            int age = 18;
             Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
             Console.ReadKey();
         }
